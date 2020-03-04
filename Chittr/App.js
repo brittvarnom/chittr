cf85d1cd27203d13a97e6e90983087f8686ffd96
brittvarnom <brittany.varnom@bbc.co.uk>
2020-03-04 10:28:45 +0000

    Added api requests for register, login, post and get user

--- Chittr/App.js
+++ Chittr/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ActivityIndicator, FlatList } from 'react-native';
+import { View, ActivityIndicator, FlatList, Button, Alert, ScrollView } from 'react-native';
 import Chit from './components/Chit';
 
 class Chittr extends Component {
@@ -13,7 +13,9 @@ class Chittr extends Component {
 			timestamp: '',
 			chit_content: '',
 			user: [],
-			location: ''
+			location: '',
+			user_token: '',
+			user_id: ''
 		};
 	}
 
@@ -25,12 +27,102 @@ class Chittr extends Component {
 					isLoading: false,
 					responseJ: responseJson,
 				});
-				console.log('TEST', responseJson);
 			})
 			.catch((error) => {
-				console.log(error);
+				console.log('>>> ERROR', error);
 			});
 	}
+
+	getUser(user_id) {
+		return fetch(`http://10.0.2.2:3333/api/v0.0.5/user/${user_id}`)
+			.then((response) => response.json())
+			.then((responseJson) => {
+				this.setState({
+					isLoading: false,
+					responseJ: responseJson,
+				});
+				console.log('>>> GET USER SUCCESS', responseJson);
+				Alert.alert(responseJson)
+			})
+			.catch((error) => {
+				console.log('>>> ERROR', error);
+			});
+	}
+
+	postUserRegister() {
+		const data = JSON.stringify({
+			given_name: 'Britt',
+			family_name: 'Test',
+			email: 'britttest@test.com',
+			password: 'password'
+		});
+
+		return fetch('http://10.0.2.2:3333/api/v0.0.5/user', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: data
+		})
+			.then((response) => response.json())
+			.then((responseJson) => {
+				//sets login token and current user id
+				console.log('>>> REGISTER SUCCESS', `Account created! Account id: ${responseJson.id}`);
+			})
+			.catch((error) => {
+				console.error('>>> ERROR', error);
+			})
+	}
+
+	postUserLogin() {
+		const data = JSON.stringify({
+			email: 'britttest@test.com',
+			password: 'password'
+		});
+
+		return fetch('http://10.0.2.2:3333/api/v0.0.5/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: data
+		})
+			.then((response) => response.json())
+			.then((responseJson) => {
+				//sets login token and current user id
+				this.setState({ user_id: responseJson.id, user_token: responseJson.token })
+				console.log('>>> LOGIN SUCCESS', `Logged in! User id: ${this.state.user_id}, Token: ${this.state.user_token}`)
+			})
+			.catch((error) => {
+				console.error('>>>> ERROR', error);
+			})
+	}
+
+	postChit(token) {
+		const data = JSON.stringify({
+			timestamp: Date.now(),
+			chit_content: "test",
+		});
+
+		return fetch('http://10.0.2.2:3333/api/v0.0.5/chits', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Authorization': token
+			},
+			body: data
+		})
+			.then((response) => response.json())
+			.then((responseJson) => {
+				//sets login token and current user id
+				// this.setState({ user_id: responseJson.id, token: responseJson.token })
+				console.log('>>> POST CHIT SUCCESS', `Posted!`)
+			})
+			.catch((error) => {
+				console.error('>>> ERROR', error);
+			})
+	}
+
 	componentDidMount() {
 		this.getLatestChits();
 	}
@@ -46,13 +138,34 @@ class Chittr extends Component {
 
 		return (
 			<View>
-				<FlatList
-					data={this.state.responseJ}
-					renderItem={({ item }) =>
-						<View>
-							<Chit item={item} />
-						</View>}
-					keyExtractor={({ id }, index) => id} />
+				<ScrollView>
+					<Button
+						title='Register'
+						onPress={() => { this.postUserRegister() }}
+					/>
+					<Button
+						title='Login'
+						onPress={() => { console.log('>>> TIME', Date.now()), this.postUserLogin() }}
+					/>
+					<Button
+						title='Me'
+						onPress={() => { this.getUser(this.state.user_id) }}
+					/>
+					<Button
+						title='Chit'
+						onPress={() => { console.log('>>> TOKEN', this.state.user_token); this.postChit(this.state.user_token) }}
+					/>
+					{/* List rendering all chits */}
+					<FlatList
+						data={this.state.responseJ}
+						// Map each item
+						renderItem={({ item }) =>
+							<View>
+								{/* Individual chit component */}
+								<Chit item={item} />
+							</View>}
+						keyExtractor={({ id }, index) => id} />
+				</ScrollView>
 			</View >
 		);
 	}
