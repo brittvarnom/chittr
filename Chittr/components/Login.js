5ebaee69abb6c4bbce66726aff506815b20c79af
brittvarnom <brittany.varnom@bbc.co.uk>
2020-03-18 20:36:51 +0000

    store login token in asyncstorage and handle logout

--- Chittr/components/Login.js
+++ Chittr/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ActivityIndicator, Button, TextInput } from 'react-native';
+import { Text, View, ActivityIndicator, Button, AsyncStorage, Alert, TextInput } from 'react-native';
 
 class Login extends Component {
 
@@ -9,34 +9,81 @@ class Login extends Component {
         this.state = {
             email: '',
             password: '',
-            loginMessage: ''
+            loginMessage: '',
+            textInputData: '',
         };
     }
 
-    postUserLogin(email, password) {
+    setValueLocally = () => {
+        AsyncStorage.setItem('@LOGIN_TOKEN', this.state.user_token);
+        Alert.alert("Value stored successfully");
+    }
+
+    getValueLocally = () => {
+        AsyncStorage.getItem('@LOGIN_TOKEN').then((value) => this.setState({ user_token: value }));
+    }
+
+    deleteValueLocally = () => {
+        try {
+            AsyncStorage.removeItem('@LOGIN_TOKEN');
+        }
+        catch (exception) {
+            return;
+        }
+    }
+
+    async postUserLogin(email, password) {
         const data = JSON.stringify({
             email: email,
             password: password
         });
 
-        return fetch('http://10.0.2.2:3333/api/v0.0.5/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: data
-        })
-            .then((response) => response.json())
-            .then((responseJson) => {
-                //sets login token and current user id
-                this.setState({ user_id: responseJson.id, user_token: responseJson.token })
-                console.log('>>> LOGIN SUCCESS', `Logged in! User id: ${this.state.user_id}, Token: ${this.state.user_token}`)
-                this.setState({ loginMessage: '' });
-            })
-            .catch((error) => {
-                this.setState({ loginMessage: 'Failed to log in, wrong email/password' });
-                console.log('>>> LOGIN FAILED', `Failed to login - wrong email/password. ${error}`);
-            })
+        try {
+            const response = await fetch('http://10.0.2.2:3333/api/v0.0.5/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: data
+            });
+            const responseJson = await response.json();
+            //sets login token and current user id
+            this.setState({ user_id: responseJson.id, user_token: responseJson.token });
+            console.log('>>> LOGIN SUCCESS', `Logged in! User id: ${this.state.user_id}, Token: ${this.state.user_token}`);
+            this.setValueLocally();
+            console.log('>>>>>>>>>>>>>>>>', this.props.navigation)
+            this.props.navigation.goBack();
+        }
+        catch (error) {
+            this.setState({ loginMessage: 'Failed to log in, wrong email/password' });
+            console.log('>>> LOGIN FAILED', `Failed to login - wrong email/password. ${error}`);
+        }
+    }
+
+    async postUserLogout() {
+        console.log('LOGOUT DEBUG: TOKEN:', this.state.user_token);
+        try {
+            await fetch('http://10.0.2.2:3333/api/v0.0.5/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Authorization': this.state.user_token
+                }
+            });
+            this.deleteValueLocally();
+            this.props.navigation.goBack();
+        }
+        catch (error) {
+            this.deleteValueLocally();
+            this.setState({ loginMessage: `Failed to log out, ${error}` });
+            console.log('>>> LOGOUT ERROR: ', error);
+        }
+        this.setState({ user_token: null });
+        console.log('>>> LOGOUT ', `Logged out! Token: ${this.state.user_token}`);
+    }
+
+    componentDidMount() {
+        this.getValueLocally();
     }
 
     render() {
@@ -46,6 +93,14 @@ class Login extends Component {
                     <ActivityIndicator />
                 </View>
             )
+        }
+
+        if (this.state.user_token != "" && this.state.user_token != undefined) {
+            console.log('DBUEGGGGG: ', this.state.user_token);
+            return <View>
+                <Text>You are already logged in :)</Text>
+                <Button title='Log out' onPress={() => { this.postUserLogout(this.state.user_token) }} />
+            </View>
         }
 
         return (
